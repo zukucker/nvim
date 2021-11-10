dac6c846ac7a947f24ec19111af4c0b92d1ff12b
zukucker <alexander.froehling@googlemail.com>
2021-11-10 10:09:44 +0100

    added install script for neovim

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#nvim installation
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+git clone https://github.com/neovim/neovim/ $HOME/GIT/
+cd $HOME/GIT/neovim/
+make
+sudo make install
+
+
+
+#language setup
 #setup for php
 mkdir -p $HOME/GIT/
 cd $HOME/GIT/
